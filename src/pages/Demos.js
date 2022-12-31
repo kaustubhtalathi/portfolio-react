97f27f2ceb5f1d219706e403d3db3ebad6380f00
kaustubhtalathi <kaustubhtalathi@gmail.com>
2022-12-30 20:49:07 -0800

    Update Demos.js

--- src/pages/Demos.js
+++ src/pages/Demos.js
@@ -6,30 +6,38 @@ function Demos() {
         {
             id: 1,
             title: "React - Github profiler",
-            imageUrl:
-                "/assets/images/project-screenshots/sprint_allin_deviceconfigurator.png",
+            imageUrl: "",
             url: "http://ktalathi.com/demos/github/",
         },
         {
             id: 2,
             title: "Flexbox - Chessboard",
-            imageUrl:
-                "/assets/images/project-screenshots/sprint_bundle_devicewall.png",
+            imageUrl: "",
             url: "http://ktalathi.com/demos/flexbox/",
         },
         {
             id: 3,
             title: "React - Github finder",
-            imageUrl:
-                "/assets/images/project-screenshots/sprint_allin_deviceconfigurator.png",
+            imageUrl: "",
             url: "",
         },
         {
             id: 4,
             title: "React - Feedback App",
-            imageUrl:
-                "/assets/images/project-screenshots/sprint_allin_deviceconfigurator.png",
+            imageUrl: "",
             url: "",
+        },
+        {
+            id: 3,
+            title: "React - YOLO Stock sentiments",
+            imageUrl: "",
+            url: "https://master.d10feblik8amou.amplifyapp.com/",
+        },
+        {
+            id: 4,
+            title: "VueJS - ToDo App",
+            imageUrl: "",
+            url: "https://main.d9f81ain3jz5c.amplifyapp.com/",
         },
     ];
 
