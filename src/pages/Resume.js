19039be8dc5d2fb161d81b3e6b92a477b97b71e7
kaustubhtalathi <kaustubhtalathi@gmail.com>
2023-01-01 13:27:59 -0800

    resume error scenario

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -6,9 +6,28 @@ import { Document, Page } from "react-pdf/dist/cjs/entry.webpack";
 
 function Resume() {
     const [numPages, setNumPages] = useState(null);
+    const [isError, setIsError] = useState(false);
 
     function onDocumentLoadSuccess({ numPages }) {
+        setIsError(false);
         setNumPages(numPages);
+    }
+
+    function onDocumentLoadError() {
+        setIsError(true);
+    }
+
+    if (isError) {
+        return (
+            <>
+                <Title titleText={"Resume"} />
+
+                <p className="text-center">
+                    There was an error to load the document. Please contact me
+                    on Linkedin.
+                </p>
+            </>
+        );
     }
 
     return (
@@ -19,6 +38,8 @@ function Resume() {
                 file="/assets/pdf/resume.pdf"
                 options={{ workerSrc: "/pdf.worker.js" }}
                 onLoadSuccess={onDocumentLoadSuccess}
+                onLoadError={onDocumentLoadError}
+                onDocumentLoadErrorr={onDocumentLoadError}
             >
                 {Array.from(new Array(numPages), (el, index) => (
                     <Page key={`page_${index + 1}`} pageNumber={index + 1} />
