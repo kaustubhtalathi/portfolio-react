94598cee5935aea173dee54a7591788358591668
kaustubhtalathi <kaustubhtalathi@gmail.com>
2022-12-31 17:39:36 -0800

    resume fix

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,28 +1,30 @@
 import Title from "../components/Title";
 
+import { useState } from "react";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
+import resumePDF from "../assets/pdf/resume.pdf";
+
 function Resume() {
+    const [numPages, setNumPages] = useState(null);
+    const [pageNumber, setPageNumber] = useState(1);
+
+    function onDocumentLoadSuccess({ numPages }) {
+        setNumPages(numPages);
+    }
+
     return (
         <>
             <Title titleText={"Resume"} />
-            <div className="h-screen">
-                <object
-                    data="/assets/pdf/resume.pdf"
-                    type="application/pdf"
-                    width="100%"
-                    className="h-full"
-                >
-                    <p>
-                        Cannot load resume: Please visit{" "}
-                        <a
-                            href="https://www.linkedin.com/in/kaustubh-talathi-5a3a088/"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            linkedin profile:
-                        </a>
-                    </p>
-                </object>
-            </div>
+
+            <Document
+                file={resumePDF}
+                options={{ workerSrc: "/pdf.worker.js" }}
+                onLoadSuccess={onDocumentLoadSuccess}
+            >
+                {Array.from(new Array(numPages), (el, index) => (
+                    <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+                ))}
+            </Document>
         </>
     );
 }
