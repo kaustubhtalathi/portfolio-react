f65fa483a0b93df9e18c2ec61df0f653076659d5
kaustubhtalathi <kaustubhtalathi@gmail.com>
2022-12-31 16:59:08 -0800

    added 404

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,14 @@
-import React from "react";
+import { TbError404 } from "react-icons/tb";
 
 function NotFound() {
-    return <div>NotFound</div>;
+    return (
+        <div className="flex flex-col items-center gap-10">
+            <div>
+                <TbError404 className="text-6xl" />
+            </div>
+            <div>404 Not Found</div>
+        </div>
+    );
 }
 
 export default NotFound;
